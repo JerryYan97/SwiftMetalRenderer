b74f5840a4a44a523c8ea267470e3b5ba317c90c
JerryYan97 <yanjerryjiarui@gmail.com>
2025-03-11 23:28:10 -0400

    Initial Commit

---
+++ SwiftMetalRenderer/SwiftMetalRendererApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMetalRendererApp.swift
+//  SwiftMetalRenderer
+//
+//  Created by Jiarui Yan on 3/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMetalRendererApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
